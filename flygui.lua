8f7394a166a8399fe41ac92f2ff9f665bf8ba951
BlobRaccoon <sjrogers2024@gmail.com>
2026-06-14 04:49:57 -0600

    Update flygui.lua

--- flygui.lua
+++ flygui.lua
@@ -178,3 +178,6 @@ CreateButton("Monkey gang", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/agarvsocooolsmith/Scripts/refs/heads/main/MONKEYGANGUI.lua"))()"))()
 end)
 
+CreateButton("Script Search", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlobRaccoon/fly-gui/refs/heads/main/search.lua"))()"))()
+end)
